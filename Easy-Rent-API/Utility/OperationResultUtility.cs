43345049de680f5c5a7f75b693be1def5087ee18
AntonionIQ <163115959+AntonionIQ@users.noreply.github.com>
2024-04-02 14:31:27 +0200

    Create OperationResultUtility.cs

---
+++ Easy-Rent-API/Utility/OperationResultUtility.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Classi.Utility
+{
+    public class OperationResult(bool isSuccess, string message = "")
+    {
+        public bool IsSuccess { get; } = isSuccess;
+        public string Message { get; } = message;
+    }
+
+    public class OperationResult<T>(bool isSuccess, string message, T? data = default) : OperationResult(isSuccess, message)
+    {
+        public T Data { get; } = data;
+    }
+
+}
